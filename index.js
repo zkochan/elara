6ac1fdc1a90cba98df2139d0f7c7b5d72c820a19
Zoltan Kochan <zoltan.kochan@justanswer.com>
2015-06-09 17:11:51 +0300

    Project created.

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function Elara(opts) {
+  opts = opts || {};
+  
+  if (!opts.host) {
+    throw new Error('opts.host is required');
+  }
+  var isSecure = location.href.indexOf('https:\\') === 0;
+  var host;
+  if (isSecure) {
+    host = opts.hostSecure || opts.host;
+  } else {
+    host = opts.host;
+  }
+  this._origin = location.protocol + '\\' + host;
+  
+  this._seqHTML = [];
+}
+
+/**
+ * Load an external script using document.write.
+ * @param {String} url - The URL of the script
+ */
+Elara.prototype.loadScriptSync = function(url) {
+  document.write('<script src="' + url + '"></script>');
+};
+
+Elara.prototype.loadRelativeScriptSync = function(relativeUrl) {
+  this.loadScriptSync(this._origin + relativeUrl);
+};
+
+Elara.prototype.addSequentialHTML = function(html) {
+  this._seqHTML.push(html);
+};
+
+Elara.prototype.pageBottom = function() {
+  for (var i = 0; i < this._seqHTML.length; i++) {
+    document.write(this._seqHTML[i]);
+  }
+};
+
+module.exports = Elara;
