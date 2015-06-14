e75d0e52553a233ea658dd15f0250649551128f2
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-06-14 16:07:07 +0300

    Removed adding relative scripts from Elara.

--- test/index.js
+++ test/index.js
@@ -5,7 +5,6 @@ var Elara = require('../lib');
 describe('Elara', function() {
   it('loadScriptSync', function(done) {
     var elara = new Elara({
-      host: 'localhost:3000',
       document: {
         write: function(html) {
           expect(html).toBe('<script src="http://domain.com/smth.js"></script>');
@@ -18,7 +17,6 @@ describe('Elara', function() {
 
   it('addSequentialHTML', function(done) {
     var elara = new Elara({
-      host: 'localhost:3000',
       document: {
         write: function(html) {
           expect(html).toBe('foobar');
@@ -29,56 +27,5 @@ describe('Elara', function() {
     elara.addSequentialHTML('foo');
     elara.addSequentialHTML('bar');
     elara.pageBottom();
-  });
-
-  describe('loadRelativeScriptSync', function() {
-    it('on non-secure page', function(done) {
-      var elara = new Elara({
-        host: 'localhost:3000',
-        location: {
-          protocol: 'http:'
-        },
-        document: {
-          write: function(html) {
-            expect(html).toBe('<script src="http://localhost:3000/smth.js"></script>');
-            done();
-          }
-        }
-      });
-      elara.loadRelativeScriptSync('/smth.js');
-    });
-
-    it('on secure page when no hostSecure specified', function(done) {
-      var elara = new Elara({
-        host: 'localhost:3000',
-        location: {
-          protocol: 'https:'
-        },
-        document: {
-          write: function(html) {
-            expect(html).toBe('<script src="https://localhost:3000/smth.js"></script>');
-            done();
-          }
-        }
-      });
-      elara.loadRelativeScriptSync('/smth.js');
-    });
-
-    it('on secure page when hostSecure specified', function(done) {
-      var elara = new Elara({
-        host: 'localhost:3000',
-        hostSecure: 'localhost:3001',
-        location: {
-          protocol: 'https:'
-        },
-        document: {
-          write: function(html) {
-            expect(html).toBe('<script src="https://localhost:3001/smth.js"></script>');
-            done();
-          }
-        }
-      });
-      elara.loadRelativeScriptSync('/smth.js');
-    });
   });
 });
